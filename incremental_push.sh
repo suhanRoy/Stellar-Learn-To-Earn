7b5124e9c3476c9a7935708ee50c5d268214c397
Suhan Roy <suhanroy@example.com>
2026-07-08 02:15:16 +0530

    chore: clean up unwanted files and artifacts for final submission

--- incremental_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-commits=($(git rev-list --reverse HEAD~53..HEAD))
-for commit in "${commits[@]}"; do
-  echo "Pushing $commit"
-  GIT_TERMINAL_PROMPT=0 git push origin $commit:refs/heads/main
-done
